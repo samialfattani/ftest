7d7afd04d03fe4821c3f8990f5c57acdc83d4be1
Sami Alfattani <Sami_alfattani@hotmail.com>
2023-05-01 18:22:22 +0100

    final version

--- app.py
+++ app.py
@@ -7,13 +7,10 @@ app = Flask(__name__)
 def hello():
     return "Hello, World!"
 
-# if __name__ == "__main__":
 
 if __name__ == '__main__':
     p = os.environ.get('PORT')
     p = p if p else '5051'
     print(f"https://localhost:{p}")
     print(f"https://127.0.0.1:{p}")
-    # from waitress import serve
-    # serve(app, host='0.0.0.0', port=p) # Waitress
-    app.run() # default
+    app.run(port=p) # default
